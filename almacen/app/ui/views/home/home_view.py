2ee2ab12ad24893f3947e437cf86cfda897117be
Javier Sarmiento <jsdash10000@gmail.com>
2026-04-17 19:13:38 -0700

    Inicio de la aplicacion almacne

---
+++ almacen/app/ui/views/home/home_view.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import Label
+
+class HomeView:
+    def __init__(self, root) -> None:
+        self.root = root
+        
+    
+    def run(self):
+        tk.mainloop()
---
+++ almacen/main.py
@@ -0,0 +1,7 @@
+
+MODE = 0; # Mode 0 - Normal Mode    / Mode 1 - Mode Admin
+class App:
+    if MODE == 1:
+        pass
+    else:
+        pass
